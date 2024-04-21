61402d38dc29e764384edaa20e87575d9bd9f408
Seeum Faruque <seeum200@gmail.com>
2024-04-21 02:34:45 -0700

    added bash script to run all code tests + cov

---
+++ testing/score/run_code_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd ../code_tests/func5
+./func5_tests.bat
+cd ../func6
+./func6_tests.bat
+cd ../func9
+./func9_tests.bat
+cd ../func10
+./func10_tests.bat
+cd ../func11
+./func11_tests.bat
+cd ../../score
